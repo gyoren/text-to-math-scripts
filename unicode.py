ea4fe338bb8a724a75cbf12c3c5476c5a274181d
lithium-umbreon <57561746+lithium-umbreon@users.noreply.github.com>
2020-11-20 12:03:44 +0200

    Updated script

--- unicode.py
+++ unicode.py
@@ -1,9 +1,13 @@
+import os
+
 # making initial variables
 string = ""
 unicodelist = []
+output = open("output.txt", "wb")
 
 def main():
     string = input("Write text to convert here: ")
+
     for a in string:
         unicodelist.append(a.encode("unicode_escape").hex())
         # converts input into unicode escape codes and converts result into hex
@@ -18,7 +22,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script1.append(chr(int(b, 16) + 119737))
     script1final = "".join(script1)
-    print(script1final)
 
     script2 = []
     for b in unicodelist:
@@ -29,7 +32,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script2.append(chr(int(b, 16) + 119841))
     script2final = "".join(script2)
-    print(script2final)
 
     script3 = []
     for b in unicodelist:
@@ -38,7 +40,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script3.append(chr(int(b, 16) + 119945))
     script3final = "".join(script3)
-    print(script3final)
 
     script4 = []
     for b in unicodelist:
@@ -47,7 +48,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script4.append(chr(int(b, 16) + 120101))
     script4final = "".join(script4)
-    print(script4final)
 
     script5 = []
     for b in unicodelist:
@@ -56,7 +56,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script5.append(chr(int(b, 16) + 120153))
     script5final = "".join(script5)
-    print(script5final)
 
     script6 = []
     for b in unicodelist:
@@ -65,7 +64,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script6.append(chr(int(b, 16) + 120205))
     script6final = "".join(script6)
-    print(script6final)
 
     script7 = []
     for b in unicodelist:
@@ -74,7 +72,6 @@ def main():
         elif 96 < int(b, 16) < 123:
             script7.append(chr(int(b, 16) + 120257))
     script7final = "".join(script7)
-    print(script7final)
 
     script8 = []
     for b in unicodelist:
@@ -83,6 +80,17 @@ def main():
         elif 96 < int(b, 16) < 123:
             script8.append(chr(int(b, 16) + 120309))
     script8final = "".join(script8)
-    print(script8final)
 
-main() # this is put into a function in case someone would implement font selection
+    output.write(f"{script1final}\n{script2final}\n{script3final}\n{script4final}\n{script5final}\n{script6final}\n{script7final}\n{script8final}".encode("utf8"))
+
+def continuation():
+    continueQuestion = input("Continue? 'y' to continue, anything else to finish: ")
+    while continueQuestion != "" and continueQuestion == "y":
+        main()
+        continueQuestion = input("Continue? 'y' to continue, anything else to finish: ")
+        output.write("\n\n".encode("utf8"))
+
+while True: # while loop so you can write as much as you want, though the text will be separated every "session" with an empty line
+    main()
+    continuation()
+    break
